dab1eabbfa54d5850fa5231d939a51aec6465352
vanessapptim <128414664+vanessastreabs@users.noreply.github.com>
2023-03-23 11:07:40 -0300

    change assert for product added

--- src/test/java/org/appium/pages/CartPage.java
+++ src/test/java/org/appium/pages/CartPage.java
@@ -1,7 +1,6 @@
 package org.appium.pages;
 
 import org.openqa.selenium.WebElement;
-import org.testng.Assert;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
@@ -18,8 +17,7 @@ public class CartPage extends BasePage{
     //-------------------------------------------------------------------------------------------------------//
 
     //Métodos    
-    public void validateProduct(String productName){
-        String addedProductName = productAdded.getText();
-        Assert.assertEquals(addedProductName,productName);
+    public String getProductAddedText(){
+        return productAdded.getText();
     }
 }
--- src/test/java/org/appium/tests/Test1.java
+++ src/test/java/org/appium/tests/Test1.java
@@ -3,6 +3,7 @@ package org.appium.tests;
 import org.appium.pages.CartPage;
 import org.appium.pages.HomePage;
 import org.appium.pages.ProductsPage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Test1 extends TestBase{
@@ -10,14 +11,18 @@ public class Test1 extends TestBase{
     @Test
     public void testOne(){
         HomePage homePage = new HomePage(driver);
+        ProductsPage productsPage = new ProductsPage(driver);
+        CartPage cartPage = new CartPage(driver);
+
         homePage.enterName("Felipe");
         homePage.selectCountry("Austria");
         homePage.selectGender("Male");
         homePage.tapLetsShop();
-        ProductsPage productsPage = new ProductsPage(driver);
+
         productsPage.selectProduct("PG 3");
         productsPage.tapCart();
-        CartPage cartPage = new CartPage(driver);
-        cartPage.validateProduct("PG 3");
+
+        Assert.assertEquals(cartPage.getProductAddedText(),"PG 3");
+
     }
 }

0d1e39a4934815601ba76f77f0177c87c0424991
sofisanabria <sofiasanabria28@yahoo.es>
2023-04-17 14:34:06 -0300

    Funciona

--- src/test/java/org/appium/tests/TestBase.java
+++ src/test/java/org/appium/tests/TestBase.java
@@ -26,19 +26,19 @@ public class TestBase {
         // Además setea la dirección IP y el número de puerto para el servidor de Appium.
         service = new AppiumServiceBuilder()
                 .withAppiumJS(new File(path + "\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js"))
-                .withIPAddress("127.0.0.1").usingPort(4723).build();
+                .withIPAddress("0.0.0.0").usingPort(4723).build();
         // Inicia el servicio de Appium
         service.start();
 
         // Crea una instancia de UiAutomator2Options y la configura con el nombre del dispositivo y el archivo APK para la aplicación que se va a probar.
         UiAutomator2Options options = new UiAutomator2Options();
-        options.setDeviceName("Pixel4");
+        options.setDeviceName("DemoPixel4");
         // Levanta el emulador donde se ejecutaran las pruebas
-        options.setAvd("Pixel4");
-        options.setApp(path + "\\taller2023\\taller-appium-2023\\src\\test\\java\\resources\\General-Store.apk");
+        options.setAvd("DemoPixel4");
+        options.setApp(path + "\\taller2023\\src\\test\\java\\resources\\General-Store.apk");
 
         // Crea una instancia de AndroidDriver e inicialíza con la URL del servidor de Appium y la instancia de UiAutomator2Options.
-        driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
+        driver = new AndroidDriver(new URL("http://0.0.0.0:4723"), options);
 
         // Establece el tiempo de espera implícito para el controlador.
         driver.manage().timeouts().implicitlyWait(ofSeconds(50));
